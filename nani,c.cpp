c01bcc5d0661f231933fc6393416952a6d07407f
github-sravani15 <132288130+github-sravani15@users.noreply.github.com>
2023-05-24 20:23:35 +0530

    Add files via upload

---
+++ nani,c.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the value of a=");
+	scanf("%x",&a);
+	printf("The value of a in Decimal %d",a);
+	printf("\nThe value of a in HexaDecimal %x",a);
+	
+	return 0;
+}
